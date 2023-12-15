932eb60051a0da87cf7bab6537ffcda82d5f2489
joseph patrick zoll <jpzoll@syr.edu>
2023-12-14 20:54:27 -0500

    Initial Commit

---
+++ AITesting/AITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITestingApp.swift
+//  AITesting
+//
+//  Created by Joe Zoll on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
