2264ea6df599efa9a69b20bc4715bc9834653c7d
joseph patrick zoll <jpzoll@syr.edu>
2023-12-15 19:19:02 -0500

    SwiftUI-OpenAI-API-Tutorial repository creation

--- AITesting/ContentView.swift
+++ AITesting/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import OpenAI
+
+
 
 struct ContentView: View {
+    @State private var userInput: String = ""
+    @State private var outputText: String = ""
+    
+    let openAI = OpenAI(apiToken: "insert API key here") // Make sure you have an instance of OpenAI
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            TextField("Ask me something...", text: $userInput)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Send") {
+                // Call a function to send the query to OpenAI
+                sendQuery()
+            }
+            .padding()
+            
+            Text("Output: \(outputText)")
+                .padding()
         }
         .padding()
+    }
+
+    // Function to send the query to OpenAI
+    private func sendQuery() {
+        let query = ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: .user, content: userInput)])
+        
+        openAI.chats(query: query) { result in
+            switch result {
+            case .success(let chatResult):
+                DispatchQueue.main.async {
+                    // Update UI with the assistant's response
+                    outputText = chatResult.choices.first?.message.content ?? "No response"
+                }
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
